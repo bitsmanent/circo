49903a48cb75cef67dceb0cdbd474406190e72a1
Claudio Alessi <smoppy@gmail.com>
2023-07-01 00:21:42 +0200

    Use release CFLAGS by default.

--- config.mk
+++ config.mk
@@ -9,8 +9,8 @@ MANPREFIX = ${PREFIX}/share/man
 
 # flags
 CPPFLAGS = -D_DEFAULT_SOURCE -D_POSIX_C_SOURCE=2 -DVERSION=\"${VERSION}\"
-CFLAGS   = -std=c99 -g -pedantic -Wall -O0 ${CPPFLAGS} -DDEBUG
-#CFLAGS  = -std=c99 -pedantic -Wall -Wno-deprecated-declarations -Os ${CPPFLAGS}
+#CFLAGS   = -std=c99 -g -pedantic -Wall -O0 ${CPPFLAGS} -DDEBUG
+CFLAGS  = -std=c99 -pedantic -Wall -Wno-deprecated-declarations -Os ${CPPFLAGS}
 
 # compiler and linker
 CC = cc
